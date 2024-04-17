6ede5d361f2ae333d8b40c388da65c6f37cfed1b
Abmael Rodrigo <abmaelrodrigo@MacBook-Air-de-Abmael.local>
2024-04-17 18:00:45 -0300

    adding parametrization and running tests in parallel

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,27 +2,31 @@
 These tests cover DuckDuckGo searches.
 """
 
+import pytest
+
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase', ['selenium', 'python', 'pola bear'])
+def test_basic_duckduckgo_search(browser, phrase):
   search_page = DuckDuckGoSearchPage(browser)
   result_page = DuckDuckGoResultPage(browser)
-  PHRASE = "Selenium"
+  
 
   # Given the DuckDuckGo home page is displayed
   search_page.load()
 
-  # When the user searches for "Selenium"
-  search_page.search(PHRASE)
+  # When the user searches for the phrase
+  search_page.search(phrase)
 
-  # Then the search result title contains "Selenium"
-  assert PHRASE in result_page.title()
+  # Then the search result query is the phrase
+  assert phrase == result_page.search_input_value()
 
-  # And the search result query is "Selenium"
-  assert PHRASE == result_page.search_input_value()
-
-  # And the search result links pertain to "Selenium"
+  # And the search result links pertain to the phrase
   for title in result_page.result_link_titles():
-    assert PHRASE.lower() in title.lower()
+    assert phrase.lower() in title.lower()
+
+  # And the search result title contains the phrase
+  # (Putting this assertion last guarantees that the page title will be ready)
+  assert phrase in result_page.title()
 
