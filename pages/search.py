9d8d93070c0c009360b516855a6322802094dcd4
Abmael Rodrigo <abmaelrodrigo@MacBook-Air-de-Abmael.local>
2024-04-19 17:38:56 -0300

    adding test case for search by clicking the button instead of typing RETURN

--- pages/search.py
+++ pages/search.py
@@ -14,6 +14,7 @@ class DuckDuckGoSearchPage:
 
     # Locators
     SEARCH_INPUT = (By.ID, 'searchbox_input')
+    SEARCH_BUTTON = (By.CLASS_NAME, 'searchbox_searchButton__F5Bwq')
 
     # Initializer
     def __init__(self, browser):
@@ -38,3 +39,12 @@ class DuckDuckGoSearchPage:
         The addition of Keys.RETURN will send the ENTER/RETURN key as well
         """
         search_input.send_keys(phrase + Keys.RETURN)
+
+    def type_on_search_bar(self,phrase):
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase)
+    
+    def click_on_search_button(self):
+        search_button = self.browser.find_element(*self.SEARCH_BUTTON)
+        search_button.click()
+
--- tests/test_search.py
+++ tests/test_search.py
@@ -3,11 +3,13 @@ These tests cover DuckDuckGo searches.
 """
 
 import pytest
+import unittest
 
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 
-@pytest.mark.parametrize('phrase', ['selenium', 'python', 'pola bear'])
+@unittest.SkipTest
+@pytest.mark.parametrize('phrase', ['selenium'])
 def test_basic_duckduckgo_search(browser, phrase):
   search_page = DuckDuckGoSearchPage(browser)
   result_page = DuckDuckGoResultPage(browser)
@@ -28,5 +30,30 @@ def test_basic_duckduckgo_search(browser, phrase):
 
   # And the search result title contains the phrase
   # (Putting this assertion last guarantees that the page title will be ready)
+  assert phrase in result_page.title()
+
+@pytest.mark.parametrize('phrase', ['pola bear'])
+def test_duckduckgo_search_with_click(browser, phrase):
+  search_page = DuckDuckGoSearchPage(browser)
+  result_page = DuckDuckGoResultPage(browser)
+  
+
+  # Given the DuckDuckGo home page is displayed
+  search_page.load()
+
+  # When the user types for the phrase
+  search_page.type_on_search_bar(phrase)
+
+  # And click on the search button
+  search_page.click_on_search_button()
+
+  # Then the search result query is the phrase
+  assert phrase == result_page.search_input_value()
+
+  # And the search result links pertain to the phrase
+  for title in result_page.result_link_titles():
+    assert phrase.lower() in title.lower()
+
+  # And the search result title contains the phrase
   assert phrase in result_page.title()
 
